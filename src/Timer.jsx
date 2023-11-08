c977530bad298b2eb3ace7df1850eaaa706fd939
s-ja <dkstmdwl0615@naver.com>
2023-11-08 11:21:10 +0900

    timer task trial

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 function Timer() {
+  const [seconds, setSeconds] = useState(10);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    console.log("ticking");
+    if (isActive && seconds > 0) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+    } else if (isActive && seconds === 0) {
+      clearInterval(interval);
+      alert("Time is up!");
+      setIsActive(false);
+      setSeconds(10);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const handleStartClick = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleResetClick = () => {
+    setIsActive(false);
+    setSeconds(10);
+  };
+
   return (
     <div>
-      <p>남은 시간을 표시하세요</p>
-      <button>시작</button>
-      <button>리셋</button>
+      <p>남은 시간을 표시하세요 : : {seconds} 초</p>
+      <button onClick={handleStartClick}>{isActive ? "Stop" : "Start"}</button>
+      <button onClick={handleResetClick}>리셋</button>
     </div>
   );
 }
